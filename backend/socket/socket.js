27c4d150e1f134501dfa5e8e36332937a4375461
Luke Ladas <116029936+LLadas0805@users.noreply.github.com>
2025-12-07 22:50:45 -0500

    Delete backend/socket directory

--- backend/socket/socket.js
+++
@@ -1,9 +0,0 @@
-import setupUser from './user_chat.js';
-import setupTeam from './team_chat.js';
-import setupGame from './game_chat.js';
-
-export default function setupSocket(io) {
-  setupUser(io);
-  setupTeam(io);
-  setupGame(io);
-}
